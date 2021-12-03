5d7e74fbe2f4fda9f199e8e14b201f7fed017cfd
Simone Cantoni <71756284+cantooo@users.noreply.github.com>
2021-12-03 11:51:42 +0100

    Day 2, part 1

---
+++ 02/part1.js
@@ -0,0 +1,28 @@
+// Calculate the horizontal position and depth you would have after following
+// the planned course. What do you get if you multiply your final horizontal
+// position by your final depth?
+
+let input = require("fs").readFileSync("./02/input.txt", "utf8").split("\n");
+
+for (let i = 0; i < input.length; i++) {
+	input[i] = input[i].split(" ");
+	input[i][1] = parseInt(input[i][1]);
+}
+
+let horizontalPosition = 0, depth = 0;
+
+input.forEach(element => {
+	switch (element[0]) {
+		case "forward":
+			horizontalPosition += element[1];
+			break;
+		case "down":
+			depth += element[1];
+			break;
+		default:
+			depth -= element[1];
+			break;
+	}
+});
+
+console.log(horizontalPosition * depth);
