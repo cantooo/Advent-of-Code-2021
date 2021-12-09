4fed7bee80620973d74acb0282b35469f3915e3d
Simone Cantoni <71756284+cantooo@users.noreply.github.com>
2021-12-09 09:50:12 +0100

    Day 7, part 1

---
+++ 07/part1.js
@@ -0,0 +1,25 @@
+// Determine the horizontal position that the crabs can align to using the
+// least fuel possible. How much fuel must they spend to align to that position?
+
+let input = require("fs").readFileSync("./07/input.txt", "utf8").split(",");
+input.forEach((n, i) => {
+	input[i] = parseInt(n);
+});
+
+let maxPos = input.sort((a, b) => b - a)[0];
+let minFuel;
+
+for (let pos = 0; pos < maxPos; pos++) {
+	let fuelSum = 0;
+
+	for (let i = 0; i < input.length; i++) {
+		const crabPos = input[i];
+		let fuelRequired = Math.abs(crabPos - pos);
+		fuelSum += fuelRequired;
+	}
+
+	if (pos == 0) minFuel = fuelSum;
+	else if (fuelSum < minFuel) minFuel = fuelSum;
+}
+
+console.log(minFuel);
